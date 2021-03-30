f2d377ecc1cf858e81ef3e78e315f824f6361b1c
Mamin78 <aminsh78@gmail.com>
2021-03-30 11:33:32 +0430

    Tests for WorksOnStrings Class were written

--- Phase03-UnitTest/src/WorksOnStringsTest.java
+++ Phase03-UnitTest/src/WorksOnStringsTest.java
@@ -1,41 +1,32 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 
-@RunWith(Parameterized.class)
 public class WorksOnStringsTest {
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {"hello world +amin +hello -amin -son", WorksOnStrings.MINUS_WORDS_PATTERN, new ArrayList<>(Arrays.asList("amin", "son"))},
-                {"-family tree -is -complicated -because -a -few", WorksOnStrings.MINUS_WORDS_PATTERN, new ArrayList<>(Arrays.asList("family", "is", "complicated", "because", "a", "few"))},
+    @Test
+    public void findWordWithPatternTest() {
+        assertEquals(WorksOnStrings.findWordWithPattern("hello world +amin +hello -amin -son", WorksOnStrings.MINUS_WORDS_PATTERN), new ArrayList<>(Arrays.asList("amin", "son")));
+        assertEquals(WorksOnStrings.findWordWithPattern("-family tree -is -complicated -because -a -few", WorksOnStrings.MINUS_WORDS_PATTERN), new ArrayList<>(Arrays.asList("family", "is", "complicated", "because", "a", "few")));
 
-                {"hello world +amin +hello -amin -son", WorksOnStrings.PLUS_WORDS_PATTERN, new ArrayList<>(Arrays.asList("amin", "hello"))},
-                {"+family +tree +is +complicated +because +a +few", WorksOnStrings.PLUS_WORDS_PATTERN, new ArrayList<>(Arrays.asList("family", "tree", "is", "complicated", "because", "a", "few"))},
+        assertEquals(WorksOnStrings.findWordWithPattern("hello world +amin +hello -amin -son", WorksOnStrings.PLUS_WORDS_PATTERN), new ArrayList<>(Arrays.asList("amin", "hello")));
+        assertEquals(WorksOnStrings.findWordWithPattern("+family +tree +is +complicated +because +a +few", WorksOnStrings.PLUS_WORDS_PATTERN), new ArrayList<>(Arrays.asList("family", "tree", "is", "complicated", "because", "a", "few")));
 
-                {" hello world +amin +hello -amin -son", WorksOnStrings.NORMAL_WORDS_PATTERN, new ArrayList<>(Arrays.asList("hello", "world"))},
-                {" family tree is complicated because a few", WorksOnStrings.NORMAL_WORDS_PATTERN, new ArrayList<>(Arrays.asList("family", "tree", "is", "complicated", "because", "a", "few"))},
-        });
-    }
-
-    private String query;
-    private String pattern;
-    private ArrayList<String> expected;
-
-    public WorksOnStringsTest(String query, String pattern, ArrayList<String> expected) {
-        this.query = query;
-        this.pattern = pattern;
-        this.expected = expected;
+        assertEquals(WorksOnStrings.findWordWithPattern(" hello world +amin +hello -amin -son", WorksOnStrings.NORMAL_WORDS_PATTERN), new ArrayList<>(Arrays.asList("hello", "world")));
+        assertEquals(WorksOnStrings.findWordWithPattern(" family tree is complicated because a few", WorksOnStrings.NORMAL_WORDS_PATTERN), new ArrayList<>(Arrays.asList("family", "tree", "is", "complicated", "because", "a", "few")));
     }
 
     @Test
-    public void findWordWithPatternTest() {
-        assertEquals(WorksOnStrings.findWordWithPattern(this.query, this.pattern), this.expected);
+    public void normalizeTheContentTest() {
+        assertEquals(WorksOnStrings.normalizeTheContent(" Hello World! I am Amin Shafiei. What is your Plan?."),"hello world i am amin shafiei what is your plan");
+    }
+
+    @Test
+    public void deleteRedundantCharsTest() {
+        assertEquals(WorksOnStrings.deleteRedundantChars(";,?\"'#"), "");
+        assertEquals(WorksOnStrings.deleteRedundantChars("hello :amin"), "hello amin");
+        assertEquals(WorksOnStrings.deleteRedundantChars("What a pity!"), "What a pity");
     }
 }
